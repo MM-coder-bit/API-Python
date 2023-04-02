210aa8eeac2e046ec9b80e1f8ff160140c5e8324
Mateus <marques18@outlook.com>
2023-04-02 18:41:12 -0300

    adicionado rota para a consulta dos dados via API com a URL localhost

--- app.py
+++ app.py
@@ -11,27 +11,34 @@
                 # - localhost/livro/id  (DELETE)
 # 4 - Quais os recursos - Livros
 
-from flask import flask,jsonify, request
+from flask import Flask,jsonify, request
 
-app = flask(__name__)
+app = Flask(__name__)
 
 livros = [
-    {
-        'id':1,
-        'titulo':'O senhor dos Anéis - A sociedade do Anel',
-        'autor': 'J.R.R Tolkien'
-    },
-    {
-        'id':2,
-        'titulo':'Harry Potter e a Pedra Filosofal',
-        'autor': 'J.K Howling'
-    },
-    {
-        'id':3,
-        'titulo':'Hábitos Atõmicos',
-        'autor': 'James Clear'
-    },
+            {
+                'id':1,
+                'titulo':'O senhor dos Anéis - A sociedade do Anel',
+                'autor': 'J.R.R Tolkien'
+            },
+            {
+                'id':2,
+                'titulo':'Harry Potter e a Pedra Filosofal',
+                'autor': 'J.K Howling'
+            },
+            {
+                'id':3,
+                'titulo':'Hábitos Atõmicos',
+                'autor': 'James Clear'
+            },
+         ]
+# Consultar (Todos)
+@app.route('/livros',methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+    
+# Consultar (Id)
+# Editar
+# Excluir
 
-
-]
-
+app.run(port=5000,host='localhost',debug=True)
