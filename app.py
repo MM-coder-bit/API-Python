9b056832337436fc172507d86bdc04bfd43004f8
Mateus <marques18@outlook.com>
2023-04-02 19:25:42 -0300

    adicionado rota para deletar o ID selecionado

--- app.py
+++ app.py
@@ -53,20 +53,28 @@ def obter_livros_id(id):
 
 def editar_livro_id(id):
     livro_alterado = request.get_json()           # faz a requisição do JSON e adiciona na var 'livro alterado'
-    for indice,livro in enumerate(livros):        # percorre todos os livros usando o método enumerate() para incdicar qual o index atual
+    for indice,livro in enumerate(livros):        # percorre todos os livros usando o método enumerate() para indicar qual o index atual
         if livro.get('id') == id:
             livros[indice].update(livro_alterado) # atualiza com a nova informação
             return jsonify(livros[indice])
 
 # Criar
-@app.route('/livros',methods=['POST'])
+@app.route('/livros',methods=['POST']) # Adicionado rota para criar novo livro, com apenas o POST
 
 def incluir_livro():
-    novo_livro = request.get_json()
-    livros.append(novo_livro)
+    novo_livro = request.get_json()    # faz a requisição do JSON e adiciona na var 'novo livro'
+    livros.append(novo_livro)          # incluir usando o append()
 
     return jsonify(livros)
 
 # Excluir
+@app.route('/livros/<int:id>',methods=['DELETE']) # Adicionado rota para excluir livro, com apenas o DELETE
+
+def excluir_livros(id):
+    for indice,livro in enumerate(livros):        # percorre todos os livros usando o método enumerate() para indicar qual o index atual
+        if livro.get('id') == id:                 # verifica se o ID foi encontrado
+            del livros[indice]                    # delete o id indicado
+
+    return jsonify(livros)
 
 app.run(port=5000,host='localhost',debug=True) # URL para as requisições = 'http://localhost:5000/livros'
